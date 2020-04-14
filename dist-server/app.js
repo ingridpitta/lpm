aa009690003c85f412c60c9982fdbebaf4779ed8
ingridpitta <ingridpitta056@gmail.com>
2020-04-14 19:59:55 -0300

    [testing] materialize-sass config

--- dist-server/app.js
+++ dist-server/app.js
@@ -4,16 +4,28 @@ var _publicRoutes = _interopRequireDefault(require("./routes/public/publicRoutes
 
 var _express = _interopRequireDefault(require("express"));
 
+var _nodeSassMiddleware = _interopRequireDefault(require("node-sass-middleware"));
+
 var _path = _interopRequireDefault(require("path"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])(); //Middlewares
 
-app.set('views', _path["default"].join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'public')));
-app.use('/', _publicRoutes["default"]);
-app.listen(3000, function () {
-  return console.log('Running in PORT 3000');
+app.set("views", _path["default"].join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use("/style", (0, _nodeSassMiddleware["default"])({
+  /* Options */
+  src: __dirname + "/sass",
+  dest: _path["default"].join(__dirname, "public"),
+  debug: true,
+  outputStyle: "compressed"
+})); // Note: you must place sass-middleware *before* `express.static` or else it will
+// not work.
+
+app.use("/public", _express["default"]["static"](_path["default"].join(__dirname, "public"))); // app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", _publicRoutes["default"]);
+app.listen(3001, function () {
+  return console.log("Running in PORT 3001");
 });
--- server/app.js
+++ server/app.js
@@ -1,14 +1,29 @@
-import publicRoutes from './routes/public/publicRoutes';
-import express from 'express';
-import path from 'path';
+import publicRoutes from "./routes/public/publicRoutes";
+import express from "express";
+import sassMiddleware from "node-sass-middleware";
+import path from "path";
 
 const app = express();
 
 //Middlewares
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(
+  "/style",  
+  sassMiddleware({
+    /* Options */
+    src: __dirname + "/sass",
+    dest: path.join(__dirname, "public"),
+    debug: true,
+    outputStyle: "compressed",
+  })
+);
+// Note: you must place sass-middleware *before* `express.static` or else it will
+// not work.
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.use('/', publicRoutes);
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => (console.log('Running in PORT 3000')));
+app.use("/", publicRoutes);
+
+app.listen(3001, () => console.log("Running in PORT 3001"));
