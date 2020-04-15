e7cb04712d81a73a77be06433b88dcad58b8758b
ingridpitta <ingridpitta056@gmail.com>
2020-04-14 22:04:44 -0300

    [fixing] sass-materialize config [WIP]

--- dist-server/app.js
+++ dist-server/app.js
@@ -8,24 +8,24 @@ var _nodeSassMiddleware = _interopRequireDefault(require("node-sass-middleware")
 
 var _path = _interopRequireDefault(require("path"));
 
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config();
 
 var app = (0, _express["default"])(); //Middlewares
 
 app.set("views", _path["default"].join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use("/style", (0, _nodeSassMiddleware["default"])({
-  /* Options */
   src: __dirname + "/sass",
   dest: _path["default"].join(__dirname, "public"),
   debug: true,
   outputStyle: "compressed"
-})); // Note: you must place sass-middleware *before* `express.static` or else it will
-// not work.
-
-app.use("/public", _express["default"]["static"](_path["default"].join(__dirname, "public"))); // app.use(express.static(path.join(__dirname, "public")));
-
+}));
+app.use("/public", _express["default"]["static"](_path["default"].join(__dirname, "public")));
 app.use("/", _publicRoutes["default"]);
-app.listen(3001, function () {
-  return console.log("Running in PORT 3001");
+app.listen(process.env.PORT, function () {
+  return console.log("Running in PORT ".concat(process.env.PORT));
 });
--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ import publicRoutes from "./routes/public/publicRoutes";
 import express from "express";
 import sassMiddleware from "node-sass-middleware";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
 
@@ -11,19 +14,15 @@ app.set("view engine", "hbs");
 app.use(
   "/style",  
   sassMiddleware({
-    /* Options */
     src: __dirname + "/sass",
     dest: path.join(__dirname, "public"),
     debug: true,
     outputStyle: "compressed",
   })
 );
-// Note: you must place sass-middleware *before* `express.static` or else it will
-// not work.
-app.use("/public", express.static(path.join(__dirname, "public")));
 
-// app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use("/", publicRoutes);
 
-app.listen(3001, () => console.log("Running in PORT 3001"));
+app.listen(process.env.PORT, () => console.log(`Running in PORT ${process.env.PORT}`));
