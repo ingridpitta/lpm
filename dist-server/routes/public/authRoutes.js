6042fad60faca8b44d35903d5d9e2867ba8f1ad6
ingridpitta <ingridpitta056@gmail.com>
2020-04-15 21:57:47 -0300

    [config] routes add and basic config

--- dist-server/routes/public/authRoutes.js
+++ dist-server/routes/public/authRoutes.js
@@ -1,1 +1,21 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var router = _express["default"].Router();
+
+router.get("/login", function (req, res) {
+  res.render("public/login");
+});
+router.get("/signup", function (req, res) {
+  res.render("public/signup");
+});
+var _default = router;
+exports["default"] = _default;
--- server/app.js
+++ server/app.js
@@ -1,28 +1,84 @@
-import publicRoutes from "./routes/public/publicRoutes";
+/* eslint-disable quotes */
 import express from "express";
 import sassMiddleware from "node-sass-middleware";
 import path from "path";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import session from "express-session";
+import MongoDBStore from "connect-mongodb-session";
 
-dotenv.config()
+// Routes
+import publicRoutes from "./routes/public/publicRoutes";
+import authRoutes from "./routes/public/authRoutes";
+import chat from "./routes/private/chat";
+import dashboard from "./routes/private/dashboard";
+import profile from "./routes/private/profile";
+import registerObject from "./routes/private/registerObject";
+import registerTravel from "./routes/private/registerTravel";
+import deal from "./routes/private/deal";
+
+const MongoStore = MongoDBStore(session);
+
+dotenv.config();
 
 const app = express();
 
-//Middlewares
+// DB Connection
+mongoose
+  .connect("mongodb://localhost/task-manager", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  // eslint-disable-next-line no-console
+  .then(() => console.log("Conectado ao Banco de Dados"))
+  .catch(err => {
+    throw new Error(err);
+  });
+
+// Middlewares
+
+// Sass
 app.use(
-  "/styles",  
+  "/styles",
   sassMiddleware({
-    src: __dirname + "/sass",
+    src: `${__dirname}/sass`,
     dest: path.join(__dirname, "public", "styles"),
     debug: true,
-    outputStyle: "compressed",
+    outputStyle: "compressed"
   })
 );
-  
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Cookie
+app.use(
+  session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+);
+
+// public routes
 app.use("/", publicRoutes);
+app.use("/auth", authRoutes);
 
-app.listen(process.env.PORT, () => console.log(`Running in PORT ${process.env.PORT}`));
+// private routes
+app.use("/chat", chat);
+app.use("/dashboard", dashboard);
+app.use("/profile", profile);
+app.use("/registerObject", registerObject);
+app.use("/registerTravel", registerTravel);
+app.use("/deal", deal);
+
+// eslint-disable-next-line no-console
+app.listen(process.env.PORT, () =>
+  console.log(`Running in PORT ${process.env.PORT}`)
+);
