fe9a4eae6785e0a5d7e5eb33fb8b234a95c637dc
ingridpitta <ingridpitta056@gmail.com>
2020-04-17 22:26:56 -0300

    [new_feature]adding geolocation

--- server/public/js/location.js
+++ server/public/js/location.js
@@ -1,9 +1,4 @@
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// document.body.innerHTML = `<script src={https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}}></script>`;
-
+// Getting User Location [Origin]
 const displayLocationInfo = position => {
   const lng = position.coords.longitude;
   const lat = position.coords.latitude;
@@ -16,3 +11,19 @@ const navigatorObject = window.navigator;
 if (navigatorObject.geolocation) {
   navigatorObject.geolocation.getCurrentPosition(displayLocationInfo);
 }
+
+// Getting Objects Locations [Destination]
+const objectsLocations = () => {
+  const objects = document.getElementsByClassName("object--origin");
+  console.log({ objects });
+  if (objects.length) {
+    const loc = [...objects].map(obj => obj);
+    console.log({ loc });
+  }
+};
+objectsLocations();
+// Getting Travels Locations [Destination]
+
+// Distance Matrix Requests [Objects]
+
+// Distance Matrix Requests [Travels]
