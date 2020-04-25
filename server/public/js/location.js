7815cc3053642c2a43cac06c97b6000add89bb49
ingridpitta <ingridpitta056@gmail.com>
2020-04-25 14:10:17 -0300

    showing data on dashboard screen

--- server/public/js/location.js
+++ server/public/js/location.js
@@ -3,16 +3,83 @@ const api = axios.create({
   baseURL: BASE_URL
 });
 
-// Show data (tests)
+const getTravels = async () => {
+  await api
+    .get("/travels")
+    .then(res => {
+      const { data } = res;
+      const divTravel = document.getElementById("object");
+      let content = "";
 
-const test = async () => {
-  const objects = await api.get("/objects");
-  const travels = await api.get("/travels");
+      if (data.length) {
+        console.log({ data });
+        data.map(tr => {
+          const {
+            description,
+            origin,
+            destination,
+            departure,
+            arrival,
+            space,
+            price
+          } = tr;
 
-  console.log({ objects, travels });
+          content += `<div class="travel--container"><h3>${description}</h3>
+          <h3 class="travel--origin">${origin}</h3>
+          <h3>${destination}</h3>
+          <h3>${departure}</h3>
+          <h3>${arrival}</h3>
+          <h3>${space}</h3>
+          <h3>${price}</h3></div>`;
+        });
+      } else {
+        content += "<div><h2>Nenhuma viagem cadastrada</h2></div>";
+      }
+      divTravel.innerHTML = content;
+    })
+    .catch(err => console.log(err));
 };
 
-test();
+getTravels();
+
+const getObjects = async () => {
+  await api
+    .get("/objects")
+    .then(res => {
+      const { data } = res;
+      const divObject = document.getElementById("travel");
+      let content = "";
+
+      if (data.length) {
+        console.log({ data });
+        data.map(tr => {
+          const {
+            description,
+            origin,
+            destination,
+            departure,
+            arrival,
+            size,
+            price
+          } = tr;
+
+          content += `<div class="object--container"><h3>${description}</h3>
+          <h3 class="travel--origin">${origin}</h3>
+          <h3>${destination}</h3>
+          <h3>${departure}</h3>
+          <h3>${arrival}</h3>
+          <h3>${size}</h3>
+          <h3>${price}</h3></div>`;
+        });
+      } else {
+        content += "<div><h2>Nenhum objeto cadastrado</h2></div>";
+      }
+      divObject.innerHTML = content;
+    })
+    .catch(err => console.log(err));
+};
+
+getObjects();
 
 // Getting User Location [Origin]
 const displayLocationInfo = position => {
--- server/routes/private/dashboard.js
+++ server/routes/private/dashboard.js
@@ -1,11 +1,18 @@
 import express from "express";
+import UserObject from "../../models/UserObject";
+import Travel from "../../models/Travel";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const { user } = req;
 
+  const travels = await Travel.find();
+  const objects = await UserObject.find();
+
   res.render("private/dashboard", {
+    travels,
+    objects,
     user
   });
 });
