4fed644e69e224243721af6659e99eb122c3ac4a
ingridpitta <ingridpitta056@gmail.com>
2020-05-01 21:11:12 -0300

    [deal done]

--- server/public/js/deal.js
+++ server/public/js/deal.js
@@ -3,15 +3,53 @@ const api = axios.create({
   baseURL: BASE_URL
 });
 
-
-document.querySelectorAll(".btn-select").forEach(btn => {
-  btn.onclick = async e => {
-    const { id } = await e.target;
-    console.log({ id, container: container.attributes.data });
-  };
-});
-
-const postDeal = (user2, travel, userObject) => {
+const postDeal = data => {
+  api
+    .post("/deal", data)
     .then(res => console.log(res))
     .catch(err => console.log(err));
 };
+
+document.querySelectorAll(".card-action").forEach(btn => {
+  btn.onclick = e => {
+    const { id } = e.target;
+    const url = window.location.pathname;
+
+    const type = url.split("/")[2];
+    const type_id = url.split("/")[3];
+
+    if (type === "object") {
+      api
+        .get(`/travels/${id}`)
+        .then(async res => {
+          const data = {
+            user2: res.data.user,
+            travel: res.data._id,
+            userObject: type_id
+          };
+
+          await postDeal(data);
+          console.log({ data });
+        })
+        .catch(err => console.log(err));
+    }
+
+    if (type === "travel") {
+      api
+        .get(`/objects/${id}`)
+        .then(async res => {
+          const data = {
+            user2: res.data.user,
+            travel: type_id,
+            userObject: res.data._id
+          };
+
+          await postDeal(data);
+          console.log({ data });
+        })
+        .catch(err => console.log(err));
+    }
+
+    console.log({ id, url, type, type_id });
+  };
+});
